7cb78e88444baaba676a17de7f087ac56fea76b0
guru <papu.meow@gmail.com>
2019-06-13 16:21:27 +0530

    gurus1st

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,14 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        OddEven ob=new OddEven();
+	System.out.println(ob.Checkeven(Integer.parseInt(args[0])));
+    }
+}
---
+++ src/main/java/com/mkyong/hashing/OddEven.java
@@ -0,0 +1,12 @@
+package com.mkyong.hashing;
+public class OddEven
+{
+    public String Checkeven( int x )
+    {
+       
+      if (x % 2 == 0)
+         return("The number is even.");
+      else
+         return("The number is odd.");
+    }
+}
